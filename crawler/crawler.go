6ce0b0c4d3d54a18cba9d806827e1430f1f90c82
miloud <miloamg@gmail.com>
2024-05-05 23:49:57 +0100

    added waitgroups

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,13 +1,22 @@
 package crawler
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 )
 
-func Crawl(done <-chan interface{}, depth int, urlStream string, out chan string) {
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+func Crawl(done <-chan interface{}, depth int, urlStream string, out chan string, w *sync.WaitGroup, fetcher Fetcher) {
+	defer w.Done()
 	if depth <= 0 {
 		return
 	}
@@ -23,17 +32,39 @@ func Crawl(done <-chan interface{}, depth int, urlStream string, out chan string
 		return
 	}
 
-	html := getHtml(done, urlStream)
-	u := GenerateUrls(done, html)
-	go Crawl(done, depth-1, <-u, out)
-
-	for {
-		select {
-		case <-done:
-			return
-		case val := <-u:
-			out <- val
+	_, urls, err := fetcher.Fetch(urlStream)
+	if err != nil {
+		fmt.Fprintln(os.Stdout, "err fetching", err)
+		return
+	}
+	u := make(chan string)
+	go func() {
+		defer close(u)
+		for _, ur := range urls {
+			select {
+			case <-done:
+				return
+			case u <- ur:
+			}
 		}
+	}()
+	// html := getHtml(done, urlStream)
+	// u := GenerateUrls(done, html)
+
+	// for {
+	// 	select {
+	// 	case <-done:
+	// 		return
+	// 	case val := <-u:
+	// 		w.Add(1)
+	// 		go Crawl(done, depth-1, val, out, w, fetcher)
+	// 		out <- val
+	// 	}
+	// }
+	for url := range u {
+    w.Add(1)
+		go Crawl(done, depth-1, url, out, w, fetcher)
+		out <- url
 	}
 }
 
--- main.go
+++ main.go
@@ -1,17 +1,77 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net/http"
+	"fmt"
+	"sync"
 
 	"github.com/milbmr/web-crawler/crawler"
 )
 
 func main() {
-  url := make(chan string)
-  done := make(chan interface{})
-  defer close(done)
+	var w sync.WaitGroup
+	url := make(chan string)
+	done := make(chan interface{})
+	defer close(done)
+	w.Add(2)
+	defer w.Done()
+	go crawler.Crawl(done, 2, "http://golang.org/", url, &w, fetcher)
 
-  crawler.Crawl()
+	go func() {
+		defer w.Done()
+		for u := range url {
+			fmt.Println(u)
+		}
+	}()
+
+	w.Wait()
+  fmt.Println("exe")
+	close(url)
+}
+
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"http://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"http://golang.org/pkg/",
+			"http://golang.org/cmd/",
+		},
+	},
+	"http://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/cmd/",
+			"http://golang.org/pkg/fmt/",
+			"http://golang.org/pkg/os/",
+		},
+	},
+	"http://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/pkg/",
+		},
+	},
+	"http://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/pkg/",
+		},
+	},
 }
